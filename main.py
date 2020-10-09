d74306de73de50a3b7cb4d300be6a15d1149c1b1
Aryan Pandey <61160074+AryanGIT1@users.noreply.github.com>
2020-10-10 03:17:53 +0530

    3 problems done

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 #main code for the backend
+from flask import Flask, render_template, request
+import requests
+import json
+
+app = Flask(__name__)
+
+# app.config['SQLALCHEMY_DATABSE_URI'] = 'sqlite:///database.db'
+
+# db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    r = requests.get('https://api.rootnet.in/covid19-in/contacts')
+    r = r.json()["data"]["contacts"]["regional"]
+    q = requests.get('https://api.rootnet.in/covid19-in/notifications')
+    q = q.json()["data"]["notifications"]
+    h = requests.get('https://api.rootnet.in/covid19-in/hospitals/beds')
+    h = h.json()["data"]["regional"]
+    m = requests.get('https://api.rootnet.in/covid19-in/hospitals/medical-colleges')
+    m  = m.json()["data"]["medicalColleges"]
+    return render_template('index.html', r = r, q= q, h = h, m =m )
+
+if __name__ == "__main__":
+    app.run(debug=True)
